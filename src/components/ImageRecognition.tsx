1e2b89095e469e6cadf8bfa8cee707ef5b09a10f
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-11-17 18:24:31 +0100

    Working on fixing word recognition

--- src/components/ImageRecognition.tsx
+++ src/components/ImageRecognition.tsx
@@ -1,8 +1,45 @@
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import Tesseract from "tesseract.js";
 import { resourceLimits } from "worker_threads";
+
+// filter the results
+// might need to move this elsewhere
+const filterParsedResults = (
+  input: Tesseract.RecognizeResult,
+  setState: Dispatch<SetStateAction<string[]>>
+) => {
+  let arrayText = [];
+  for (let x = 0; x < input.data.lines.length; x++) {
+    // all player data starts with 0 0 0 0 0 indicating the player scores at the start of the game
+    if (input.data.lines[x].text.includes("0 0 0 0 0")) {
+      // the word data we are looking for is at index 6 of the words array within results
+      if (input.data.lines[x].words[6].text.length != 1) {
+        // we want to filter anything that has one word results - these normally indicate player has not loaded and the OCR has not been able to read the value.
+        arrayText.push(input.data.lines[x].words[6].text);
+      }
+    }
+  }
+  console.log(arrayText);
+  setState(arrayText);
+
+  return arrayText;
+};
+
+const fixWords = (input: string) => {
+  const wordArray = input.split("");
+
+  if (wordArray[0] === "O") {
+    wordArray.splice(0, 1);
+    wordArray.join(" ");
+  } else {
+    wordArray.join(" ");
+  }
+
+  console.log(wordArray);
+};
+
 const TextRecognition = ({ selectedImage }: { selectedImage: string }) => {
-  const [recognizedText, setRecognizedText] = useState("");
+  const [recognizedText, setRecognizedText] = useState<any>([]);
   useEffect(() => {
     const recognizeText = async () => {
       if (selectedImage) {
@@ -10,14 +47,11 @@ const TextRecognition = ({ selectedImage }: { selectedImage: string }) => {
 
         console.log(result.data);
 
-        let arrayText = [];
-        for (let x = 0; x < result.data.lines.length; x++) {
-          if (result.data.lines[x].text.includes("0 0 0 0 0")) {
-            arrayText.push(result.data.lines[x].words[6]);
-          }
-        }
-        console.log(arrayText);
-        setRecognizedText(result.data.text);
+        const wordArray = filterParsedResults(result, setRecognizedText);
+
+        wordArray.forEach((word) => {
+          fixWords(word);
+        });
       }
     };
     recognizeText();
@@ -25,7 +59,11 @@ const TextRecognition = ({ selectedImage }: { selectedImage: string }) => {
   return (
     <div>
       <h2>Recognized Text:</h2>
-      <p>{recognizedText}</p>
+      <ul>
+        {recognizedText.map((item: string, index: number) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 };
