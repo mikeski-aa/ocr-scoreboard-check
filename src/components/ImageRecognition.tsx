849676509a0e1d4db42666d365ab87a98a091940
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-11-17 18:34:30 +0100

    Filtering needs to be more robust, very weak atm

--- src/components/ImageRecognition.tsx
+++ src/components/ImageRecognition.tsx
@@ -28,7 +28,15 @@ const filterParsedResults = (
 const fixWords = (input: string) => {
   const wordArray = input.split("");
 
-  if (wordArray[0] === "O" || wordArray[0] === "35") {
+  if (
+    (wordArray[0] === "O" &&
+      !(
+        wordArray[1] === "S" ||
+        wordArray[1] === "3" ||
+        wordArray[1] === "s"
+      )) ||
+    wordArray[0] === "3"
+  ) {
     wordArray.splice(0, 1);
     const joined = wordArray.join("");
     console.log(joined);
