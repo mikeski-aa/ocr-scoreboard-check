e210bf8f00b7f97e9f923660506156167cd3216b
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-11-22 11:11:57 +0100

    Fixing display styling

--- src/components/ImageUploader.tsx
+++ src/components/ImageUploader.tsx
@@ -6,12 +6,14 @@ function ImageUploader({
   otherState: Dispatch<SetStateAction<string>>;
 }) {
   const [selectedImage, setSelected] = useState<string>("");
+  const [activeName, setActiveName] = useState<string>("");
 
   const handleImageUpload = (e: SyntheticEvent) => {
     const target = e.target as HTMLInputElement;
 
     if (target.files) {
       const image = target.files[0];
+      setActiveName(image.name);
       setSelected(URL.createObjectURL(image));
       otherState(URL.createObjectURL(image));
     }
@@ -19,19 +21,21 @@ function ImageUploader({
 
   return (
     <div className="imageUploader">
-      <div className="buttonWrapper">
-        <label htmlFor="uploadBtn" className="UploadFakeBtn">
-          Upload
-        </label>
-        <input
-          id="uploadBtn"
-          className="hiddenInput"
-          type="file"
-          accept="image/*"
-          onChange={(e) => handleImageUpload(e)}
-        />
+      <div className="buttonNameWrapper">
+        <div className="buttonWrapper">
+          <label htmlFor="uploadBtn" className="UploadFakeBtn">
+            Upload
+          </label>
+          <input
+            id="uploadBtn"
+            className="hiddenInput"
+            type="file"
+            accept="image/*"
+            onChange={(e) => handleImageUpload(e)}
+          />
+        </div>
+        <div className="fileName">{activeName}</div>
       </div>
-
       {selectedImage && (
         <img src={selectedImage} alt="selected" className="previewImg" />
       )}
