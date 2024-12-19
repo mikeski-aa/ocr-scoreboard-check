2348a14e117c1b7c6a7ef93888d1faeb95390935
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-12-19 21:53:44 +0100

    Working on improving modal interactivity

--- src/components/TutorialHowToUseModal.tsx
+++ src/components/TutorialHowToUseModal.tsx
@@ -14,44 +14,49 @@ function TutorialHowToUseModal({
   return (
     <div className="modal">
       <div className="modalContent">
-        <div className="headingTextModal">
-          How to make screenshots easier to take using Steam
-        </div>
-        <div className="text">
-          <ul>
-            <li>Open steam overlay - default keybind: Shift + Tab </li>
-            <li>Click the cog - label 1.</li>
-            <li>Go to In Game - label 2.</li>
-            <li>Set up a preferred easy to access keybind</li>
-            <li>Enable "Save an external copy of screenshots" - label 3</li>
-            <li>
-              Select preferred screenshot folder that is easily accessible -
-              label 4
-            </li>
-          </ul>
-        </div>
-        <img className="exampleImage" src={pictureExample} />
-        <div className="text">
-          <ul>
-            <li>
-              After you click upload, navigate to the screenshot folder you have
-              selected
-            </li>
-            <li>
-              Each time you click upload, your screenshot folder will be
-              pre-selected
-            </li>
-            <li>Your newest screenshot will be saved on top of the list</li>
-          </ul>
-        </div>
-        <img className="exampleImage" src={locationex} />
-        <div className="text">
-          NOTE: If you don't use steam, other 3rd party screenshot apps have
-          similar screenshot folder functionality you can set up
-        </div>
-        <button className="closeBtn" onClick={closeAction}>
-          Close
+        <button className="closeModalBtn" onClick={closeAction}>
+          X
         </button>
+        <div className="yapText">
+          <div className="headingTextModal">
+            How to make screenshots easier to take using Steam
+          </div>
+          <div className="text">
+            <ul>
+              <li>Open steam overlay - default keybind: Shift + Tab </li>
+              <li>Click the cog - label 1.</li>
+              <li>Go to In Game - label 2.</li>
+              <li>Set up a preferred easy to access keybind</li>
+              <li>Enable "Save an external copy of screenshots" - label 3</li>
+              <li>
+                Select preferred screenshot folder that is easily accessible -
+                label 4
+              </li>
+            </ul>
+          </div>
+          <img className="exampleImage" src={pictureExample} />
+          <div className="text">
+            <ul>
+              <li>
+                After you click upload, navigate to the screenshot folder you
+                have selected
+              </li>
+              <li>
+                Each time you click upload, your screenshot folder will be
+                pre-selected
+              </li>
+              <li>Your newest screenshot will be saved on top of the list</li>
+            </ul>
+          </div>
+          <img className="exampleImage" src={locationex} />
+          <div className="text">
+            NOTE: If you don't use steam, other 3rd party screenshot apps have
+            similar screenshot folder functionality you can set up
+          </div>
+          <button className="closeBtn" onClick={closeAction}>
+            Close
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/TutorialModal.tsx
+++ src/components/TutorialModal.tsx
@@ -14,46 +14,52 @@ function TutorialModal({
   return (
     <div className="modal">
       <div className="modalContent">
-        <div className="text">
-          <div className="headingTextModal">How to use the app:</div>
-          <ul>
-            <li>Take a screenshot at the START of the game</li>
-            <li>Do not crop / change it</li>
-            <li>Pictures taken with a mobile phone will not work</li>
-            <li>
-              Make sure your mouse cursor is NOT hovering over the scoreboard
-            </li>
-            <li>
-              Ideally wait until everyone has loaded in to take a screenshot
-            </li>
-            <li>
-              Enter your plane BR to let the app tell you if it is an uptier or
-              downtier
-            </li>
-            <li>
-              App remembers your plane BR between different screenshot uploads
-            </li>
-            <li>None of your screenshots or data is saved by this app</li>
-          </ul>
-        </div>
-        <img className="exampleImage" src={pictureExample} />
-        <div className="text">
-          <div className="headingTextModal">Result example:</div>
-          <ul>
-            <li>
-              Successfully processing the image will look like the below example
-            </li>
-            <li>
-              Note that you can download the censored image above and try the
-              app
-            </li>
-            <li>If you do not have any any screenshots on hand</li>
-          </ul>
-        </div>
-        <img className="exampleImage" src={examplePic} />
-        <button className="closeBtn" onClick={closeAction}>
-          Close
+        <button className="closeModalBtn" onClick={closeAction}>
+          X
         </button>
+        <div className="yapText">
+          <div className="text">
+            <div className="headingTextModal">How to use the app:</div>
+            <ul>
+              <li>Take a screenshot at the START of the game</li>
+              <li>Do not crop / change it</li>
+              <li>Pictures taken with a mobile phone will not work</li>
+              <li>
+                Make sure your mouse cursor is NOT hovering over the scoreboard
+              </li>
+              <li>
+                Ideally wait until everyone has loaded in to take a screenshot
+              </li>
+              <li>
+                Enter your plane BR to let the app tell you if it is an uptier
+                or downtier
+              </li>
+              <li>
+                App remembers your plane BR between different screenshot uploads
+              </li>
+              <li>None of your screenshots or data is saved by this app</li>
+            </ul>
+          </div>
+          <img className="exampleImage" src={pictureExample} />
+          <div className="text">
+            <div className="headingTextModal">Result example:</div>
+            <ul>
+              <li>
+                Successfully processing the image will look like the below
+                example
+              </li>
+              <li>
+                Note that you can download the censored image above and try the
+                app
+              </li>
+              <li>If you do not have any any screenshots on hand</li>
+            </ul>
+          </div>
+          <img className="exampleImage" src={examplePic} />
+          <button className="closeBtn" onClick={closeAction}>
+            Close
+          </button>
+        </div>
       </div>
     </div>
   );
